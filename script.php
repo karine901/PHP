6bd63ca11d1e979d294821127fc52b49bf1301c8
Karine Maria <km123silva2339@gmail.com>
2024-09-23 20:36:32 -0300

    feito

---
+++ script.php
@@ -0,0 +1,37 @@
+<?php 
+
+$nome($_POST)['nome'];
+$nota1= $_POST['nota1'];
+$nota2= $_POST['nota2'];
+$nota3= $_POST['nota3'];
+$nota4= $_POST['nota4'];
+
+$media = ($nota1+$nota2+$nota3+$nota4) / 4;
+
+echo "Meu nome é $nome e Minha mnota final é $média";
+
+if ($media>=6) {
+    echo "O Aluno $nome,está Aprovado!";
+    
+}
+elseif($media>=4) {
+    echo "O Aluno $nome,está Aprovado!";
+
+}else  {
+    echo "O Aluno $nome,está Reprovado!";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
